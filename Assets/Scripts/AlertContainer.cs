0ee341c2c5ea71a68a1f4e8ad3353939a8e09260
rcpelisco <rcpelisco@gmail.com>
2018-09-25 01:52:37 +0800

    Added lane detector

---
+++ Assets/Scripts/AlertContainer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlertContainer : MonoBehaviour {
+
+	public GameObject alertPrefab;
+	public GameObject alertContainer;
+	private static AlertContainer _Instance;
+
+	public static AlertContainer Instance {
+		get {
+			if(_Instance == null) {
+				_Instance = GameObject.FindObjectOfType<AlertContainer>();
+			}
+			return _Instance;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		if(alertContainer == null) {
+			alertContainer = this.gameObject;
+		}
+	}
+	
+	public static void NewAlert(string text) {
+		GameObject alert = (GameObject) Instantiate(Instance.alertPrefab);
+		alert.GetComponentInChildren<Text>().text = text;
+		alert.transform.SetParent(Instance.alertContainer.transform);
+		alert.transform.localScale = Vector3.one;
+	}
+}
--- Assets/Scripts/RightWay.cs
+++ Assets/Scripts/RightWay.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class RightWay : CarSensor {
 	
+    private bool alerted = false;
+
 	void Update () {
 		if(g != null) {
             CheckFlow();
+            return;
         }
+        alerted = false;
 	}
 
     void CheckFlow() {
         Vector3 forward = transform.forward;
         Vector3 other = g.transform.forward;
-        if(Vector3.Dot(forward, other) < 0) {
-            Debug.Log("Counter Flow");
+        
+        Debug.Log(Vector3.Dot(forward, other));
+        if(Vector3.Dot(forward, other) < -0.8f) {
+            if(!alerted) {
+                AlertContainer.NewAlert("Counter Flow");
+                alerted = true;
+            }
         }
     }
 }
