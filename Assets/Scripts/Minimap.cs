7eb724593078120e4aa0f745bb55a8e0a072c8b6
rcpelisco <rcpelisco@gmail.com>
2018-10-07 23:59:32 +0800

    Added minimap

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour {
+
+    private Transform car;
+
+    public bool rotateEnabled;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if(car != null) {
+            Vector3 newPosition = car.position;
+            newPosition.y = transform.position.y;
+            transform.position = newPosition;
+            if(rotateEnabled) {
+                transform.rotation = Quaternion.Euler(90f, car.eulerAngles.y, 0f);
+            }
+        }
+	}
+
+    void Update() {
+        if(car == null && FindObjectOfType<CustomCarUserControl>() != null)
+            car = FindObjectOfType<CustomCarUserControl>().transform;
+    }
+}
